66f891949b190f04fb2fe5e1f327ec02725d3d2a
meitoc <info@meitoc.com>
2023-07-09 00:51:42 +0700

    store

--- src/modules/small-component/Logo.jsx
+++ src/modules/small-component/Logo.jsx
@@ -4,7 +4,7 @@ export function Logo () {
         style={{margin: "5px"}}
         width="45px"
         height="45px"
-            src={"../public/logo.svg"}>
+            src={"https://fakeapi.meitoc.net/api/logo.svg"}>
         </img>
     )
 }
