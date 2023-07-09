5e55b4f58419c4f2d775e180bad404cbba7895d0
meitoc <info@meitoc.com>
2023-07-09 21:55:10 +0700

    store

--- src/modules/TopMenuSearch.jsx
+++ src/modules/TopMenuSearch.jsx
@@ -67,7 +67,7 @@ export default function TopMenuSearch() {
           }}
           value={showInput}
           onKeyUp={(event)=>{
-            alert("test");
+            alert(`test${event.code}`);
             if(event.code==="Enter") {
                 console.log(showInput);
                 setSearchInput(showInput);
