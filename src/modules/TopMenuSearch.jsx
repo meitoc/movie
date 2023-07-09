a7926a4f8e0ee38c0834cfbfccbd3faaa11709cd
meitoc <info@meitoc.com>
2023-07-09 21:50:11 +0700

    store

--- src/modules/TopMenuSearch.jsx
+++ src/modules/TopMenuSearch.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-// import { useContext } from 'react';
+import { useContext } from 'react';
 
 import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
 
-// import { ContextStatus } from '../App';
+import { ContextStatus } from '../App';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -51,14 +51,12 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function TopMenuSearch() {
-  // const {setSearchInput}=useContext(ContextStatus)
+  const {setSearchInput}=useContext(ContextStatus)
   const [showInput, setShowInput]=useState("");
-  const [test, setTest]=useState("AAA");
   return (
     <Box>
       <Search>
         <SearchIconWrapper>
-        <p>{test}</p>
           <SearchIcon />
         </SearchIconWrapper>
         <StyledInputBase
@@ -69,10 +67,10 @@ export default function TopMenuSearch() {
           }}
           value={showInput}
           onKeyUp={(event)=>{
-            setTest(event.code);//test
+            alert(event.code);
             if(event.code==="Enter") {
                 console.log(showInput);
-                // setSearchInput(showInput);
+                setSearchInput(showInput);
             }
           }}
         />
