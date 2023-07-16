4759e6cc81bf14e250584a3924417bf2c8c33b45
meitoc <info@meitoc.com>
2023-07-16 20:55:42 +0700

    movie

--- src/features/authentication/checkSession.jsx
+++ src/features/authentication/checkSession.jsx
@@ -1,49 +1,49 @@
-import axios from "axios";
-import { useEffect,useContext} from "react";
-import { ContextStatus } from "../../App";
+// import axios from "axios";
+// import { useEffect,useContext} from "react";
+// import { ContextStatus } from "../../App";
 // import { createBrowserHistory } from "history";
 
 export default function CheckSession (prop) {
     // const history=createBrowserHistory();
-    const { setLoginStatus, setServiceInfo} = useContext(ContextStatus);
-    useEffect(()=>{
-        async function checkLoginSession() {
+    // const { setLoginStatus, setServiceInfo} = useContext(ContextStatus);
+    // useEffect(()=>{
+    //     async function checkLoginSession() {
             
-            axios({
-                method: 'get',
-                baseURL: 'https://fakeapi.meitoc.net',
-                url: `/api/checktoken?token=${session}`,
-            }).then(response => {
-                const data =  response.data;
-                if(data.status==="newtoken"){
-                    localStorage.setItem('loginSession',data.data.token);
-                    setLoginStatus(true);
-                    //
-                    const themoviedb = data.data.services.find(item => item.service === "themoviedb");//load login info for layer 2
-                    console.log("Website created a newtoken");
-                    setServiceInfo(themoviedb);
-                    // setLoginStatus(true);
-                } else if(data.status==="loggedin"){
-                    setLoginStatus(true);
-                    const themoviedb = data.data.services.find(item => item.service === "themoviedb");//load login info for layer 2
-                    console.log("Website created a loggedin token");
-                    setServiceInfo(themoviedb);
-                } else {
-                    setLoginStatus(false);
-                    // setLoginStatus(false);
-                }
-            })
-            .catch(error => {
-                console.log("Error when check your session!")
-                console.log(error)
-                // setLogin(false);
-                // setLoginStatus(false);
-            }); 
-        }
-        let session = localStorage.getItem('loginSession');
-        if (session !==null & session!==undefined) checkLoginSession();
-        else setLoginStatus(false);
-    },[setLoginStatus,setServiceInfo]);
+    //         axios({
+    //             method: 'get',
+    //             baseURL: 'https://fakeapi.meitoc.net',
+    //             url: `/api/checktoken?token=${session}`,
+    //         }).then(response => {
+    //             const data =  response.data;
+    //             if(data.status==="newtoken"){
+    //                 localStorage.setItem('loginSession',data.data.token);
+    //                 setLoginStatus(true);
+    //                 //
+    //                 const themoviedb = data.data.services.find(item => item.service === "themoviedb");//load login info for layer 2
+    //                 console.log("Website created a newtoken");
+    //                 setServiceInfo(themoviedb);
+    //                 // setLoginStatus(true);
+    //             } else if(data.status==="loggedin"){
+    //                 setLoginStatus(true);
+    //                 const themoviedb = data.data.services.find(item => item.service === "themoviedb");//load login info for layer 2
+    //                 console.log("Website created a loggedin token");
+    //                 setServiceInfo(themoviedb);
+    //             } else {
+    //                 setLoginStatus(false);
+    //                 // setLoginStatus(false);
+    //             }
+    //         })
+    //         .catch(error => {
+    //             console.log("Error when check your session!")
+    //             console.log(error)
+    //             // setLogin(false);
+    //             // setLoginStatus(false);
+    //         }); 
+    //     }
+    //     let session = localStorage.getItem('loginSession');
+    //     if (session !==null & session!==undefined) checkLoginSession();
+    //     else setLoginStatus(false);
+    // },[setLoginStatus,setServiceInfo]);
   return(<>
       {prop.children}
   </>);
